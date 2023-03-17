7765d52ed098363796ae9d4d47b8ebe8b99d5e0c
badrman-exe <120106072+badrman-exe@users.noreply.github.com>
2023-03-17 22:26:10 +0200

    Update and rename .2-print_alphabet.c.swp to 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+	while (alpha != 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('z');
+	return (0);
+}
