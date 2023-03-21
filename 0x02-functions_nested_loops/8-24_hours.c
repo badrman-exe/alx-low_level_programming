b510579241f88acfb51253fb1dd0159443c81d0a
badrman-exe <120106072+badrman-exe@users.noreply.github.com>
2023-03-21 22:42:11 +0200

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * jack_bauer - function that prints every minute of the day, 24 hour clock
  * h = hour, m = minutes
@@ -10,29 +13,18 @@
  */
 void jack_bauer(void)
 {
-	int i, j, k, h;
-	for (i = 0; i <= 2; i++)
+	int h, m;
+
+	for (h = 0; h < 24; h++)
 	{
-		for (j = 0;j<=9 ; j++)
+		for (m = 0; m < 60; m++)
 		{
-			for (k = 0; k <= 5; k++)
-			{
-				for (h = 0; h <= 9; h++)
-				{
-					printf("%d%d:%d%d\n", i, j, k, h);
-					if (i == 2)
-					{
-						if (j == 3)
-						{
-							if (k == 5)
-							{
-								if (h == 9)
-									return;
-							}
-						}
-					}
-				}
-			}
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
