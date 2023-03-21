b97958b51cdddeace19b6e4469c1a200c754f7f6
badrman-exe <120106072+badrman-exe@users.noreply.github.com>
2023-03-22 00:49:32 +0200

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - Entry point.
- *
- * Return: Always 0.
- */
+* main - Entry point.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
     int sum = 0, i;
