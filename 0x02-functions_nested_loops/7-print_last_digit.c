bbffac2d11043650c738ea59001ee2c50182a82d
badrman-exe <120106072+badrman-exe@users.noreply.github.com>
2023-03-21 22:43:47 +0200

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,20 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "holberton.h"
 /**
  * print_last_digit - a function that prints the last digit of a number
- * @n: number's last digit result
+ * @nld: number's last digit result
  * Return: value of the last digit
  */
-int print_last_digit(int n)
+int print_last_digit(int nld)
 {
-	return (n % 10);
+	int pld;
+
+	pld = (nld % 10);
+
+	if (pld < 0)
+	{
+		pld = (-1 * pld);
+	}
+
+	_putchar(pld + '0');
+	return (pld);
 }
