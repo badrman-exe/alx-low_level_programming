5a88232b5c3567acb9dcece107b9af742042c498
badrman-exe <120106072+badrman-exe@users.noreply.github.com>
2023-03-21 18:36:49 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - Entry point
+ * print_alphabet - a function that prints the alphabet, in lowercase
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Succes)
  */
 void print_alphabet(void)
 {
