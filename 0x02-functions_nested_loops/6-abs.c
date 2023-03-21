0fbe1912838bd4b90a6d168c70bec2d10951e856
badrman-exe <120106072+badrman-exe@users.noreply.github.com>
2023-03-21 21:35:43 +0200

    Update 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+/**
+ * _abs - a function that computes the absolute value of an integer
+ * @ab: integer input
+ * Return: absolute value of ab
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (sqrt((n * n)));
+	}
+	else
+		return (0);
+}
