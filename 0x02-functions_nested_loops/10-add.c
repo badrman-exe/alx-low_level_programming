adf58e5b4a6afcb12ac68381a55911244793f9d9
badrman-exe <120106072+badrman-exe@users.noreply.github.com>
2023-03-21 22:05:47 +0200

    Update 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * jack_bauer - function that prints every minute of the day, 24 hour clock
+ * h = hour, m = minutes
+ * / 10 allows second digit to rotate
+ * for loop breaks before passing 24:00
+ * Return: 24 hour clock line by line
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
