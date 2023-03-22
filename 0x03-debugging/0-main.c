812dad653fdf69e5d9505df2a09f3d9d3f0d3ad9
badrman-exe <120106072+badrman-exe@users.noreply.github.com>
2023-03-22 19:20:07 +0200

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
-  * main - tests function that prints if integer is positive or negative
-  * 3 or 5 below 1024.
-  * Return: Nothing.
-  */
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
 int main(void)
 {
 	int i;
 
-	i=0;
-	void positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
+
 	return (0);
 }
