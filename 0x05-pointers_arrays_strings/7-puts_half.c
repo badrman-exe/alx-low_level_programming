0f88c88933cd6c5ae1ba93193d8dba76894d9fd9
badrman-exe <120106072+badrman-exe@users.noreply.github.com>
2023-03-27 12:54:17 +0200

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,12 +17,12 @@ void puts_half(char* str)
 	}
 	if (len % 2 == 0)
 	{
-		for (i = (len / 2) + 1; i < len; i++);
+		for (i = (len / 2); i < len; i++)
 			putchar(str[i]);
 	}
 	else
 	{
-		for (i = (len - 1) / 2; i < len; i++)
+		for (i = ((len - 1) / 2) + 1; i < len; i++)
 			putchar(str[i]);
 	}
 
