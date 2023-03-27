20075f5278119dcef0783064242c136f5f8c20fd
badrman-exe <120106072+badrman-exe@users.noreply.github.com>
2023-03-27 11:31:04 +0200

    Update 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+void print_rev(char* s)
+{
+	int i=0,j;
+
+	while (s[i] != '\0')
+		i++;
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+}
