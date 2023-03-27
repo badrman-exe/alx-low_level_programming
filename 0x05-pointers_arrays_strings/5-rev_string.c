e39dd9b11138daab9fbbc4214d8b2f58d131eb9c
badrman-exe <120106072+badrman-exe@users.noreply.github.com>
2023-03-27 12:24:09 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,11 +7,10 @@
 void rev_string(char *s)
 {
 	int len = 0, j, k = 0;
-	char *temp;
+	char temp[100];
 
 	while (s[len] != '\0')
 		len++;
-	temp = new char[len];
 	for (j = len - 1; j >= 0; j--)
 		temp[k++] = s[j];
 	for (k = 0; k < len; k++)
