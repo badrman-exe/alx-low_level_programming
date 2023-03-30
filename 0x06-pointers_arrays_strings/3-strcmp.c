52e9889a19c89f9ff3588fe245c80d09640ec4ae
badrman-exe <120106072+badrman-exe@users.noreply.github.com>
2023-03-30 06:41:15 +0200

    Update 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - Write a function that compares two strings.
+ *
+ * @s1: This is the input string
+ * @s2: This is the input string
+ *
+ * Return: If the strings are equals return "0", if not return other number
+ */
+
+int _strcmp(char* s1, char* s2)
+{
+	int i;
+
+	for (i = 0; i < s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
