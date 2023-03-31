93c6b9eacbd4ce44be84d44ce7c0423306aea323
badrman-exe <120106072+badrman-exe@users.noreply.github.com>
2023-03-31 04:33:50 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,31 +5,32 @@
  *
  * Return: String with all words capitalized.
  */
-
 char *cap_string(char *s)
 {
 	int i, j;
-	char arr[] = { 32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125 };
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	while (*(s + i) != '\0')
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
 			if (i == 0)
 			{
-				s[i] -= 32;
+				*(s + i) = *(s + i) - 32;
 			}
 			else
 			{
 				for (j = 0; j <= 12; j++)
 				{
-					if (arr[j] == s[i-1])
+					if (a[j] == *(s + i - 1))
 					{
-						s[i] -= 32;
+						*(s + i) = *(s + i) - 32;
 					}
 				}
 			}
 		}
+	i++;
 	}
 	return (s);
 }
