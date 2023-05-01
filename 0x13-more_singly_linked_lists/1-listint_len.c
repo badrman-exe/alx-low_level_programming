de8a7a395a6b1f206eb81c464cf8e3385d8c42e4
badrman-exe <120106072+badrman-exe@users.noreply.github.com>
2023-05-01 12:56:27 +0200

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,13 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
+ * listint_len - returns the number of elements in
+ * a linked list.
+ * @h: head of a list.
  *
- * Return: number of nodes
+ * Return: numbers of nodes.
  */
-size_t listint_len(const listint_t* h)
+size_t listint_len(const listint_t *h)
 {
 	size_t num = 0;
 
