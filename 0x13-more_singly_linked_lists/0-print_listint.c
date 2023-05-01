1b2746b228b705fad645a271b4a4862b915e2a11
badrman-exe <120106072+badrman-exe@users.noreply.github.com>
2023-05-01 12:55:07 +0200

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
+ * print_listint - prints all the elements of a list.
+ * @h: head of a list.
  *
- * Return: number of nodes
+ * Return: numbers of nodes.
  */
 size_t print_listint(const listint_t *h)
 {
